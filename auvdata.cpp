7f44148ad05caa785b21e4505af4ecb331d87f6e
Mikhail Kozitskii <kekouke@yandex.ru>
2021-06-19 17:21:01 +1000

    Add movable_object and auvdata c++ models

--- auvdata.cpp
+++ auvdata.cpp
@@ -1,6 +1,61 @@
 #include "auvdata.h"
 
-AUVData::AUVData()
+AUVData::AUVData(QObject *parent) : QObject(parent)
 {
+}
 
+float AUVData::high() const
+{
+    return m_high;
+}
+
+void AUVData::setHigh(float newHigh)
+{
+    if (m_high == newHigh)
+        return;
+
+    m_high = newHigh;
+    emit highChanged(m_high);
+}
+
+float AUVData::deep() const
+{
+    return m_deep;
+}
+
+void AUVData::setDeep(float newDeep)
+{
+    if (m_deep == newDeep)
+        return;
+
+    m_deep = newDeep;
+    emit deepChanged(m_deep);
+}
+
+float AUVData::speed() const
+{
+    return m_speed;
+}
+
+void AUVData::setSpeed(float newSpeed)
+{
+    if (m_speed == newSpeed)
+        return;
+
+    m_speed = newSpeed;
+    emit speedChanged(m_speed);
+}
+
+float AUVData::direction() const
+{
+    return m_direction;
+}
+
+void AUVData::setDirection(float newDirection)
+{
+    if (m_direction == newDirection)
+        return;
+
+    m_direction = newDirection;
+    emit setDirection(m_direction);
 }
--- auvdata.h
+++ auvdata.h
@@ -1,16 +1,43 @@
 #ifndef AUVDATA_H
 #define AUVDATA_H
 
+#include <qobject.h>
 
-class AUVData
+class AUVData : public QObject
 {
+    Q_OBJECT
+
+    Q_PROPERTY(float high READ high WRITE setHigh NOTIFY highChanged)
+    Q_PROPERTY(float deep READ deep WRITE setDeep NOTIFY deepChanged)
+    Q_PROPERTY(float speed READ speed WRITE setSpeed NOTIFY speedChanged)
+    Q_PROPERTY(float direction READ direction WRITE setDirection NOTIFY directionChanged)
+
   public:
-    AUVData();
-  private:
-    float high_;
-    float deep_;
-    float speed_;
-    float direction_;
+    AUVData(QObject *parent = nullptr);
+
+    float high() const;
+    void setHigh(float newHigh);
+
+    float deep() const;
+    void setDeep(float newDeep);
+
+    float speed() const;
+    void setSpeed(float newSpeed);
+
+    float direction() const;
+    void setDirection(float newDirection);
+
+signals:
+    void highChanged(float message);
+    void deepChanged(float message);
+    void speedChanged(float message);
+    void directionChanged(float message);
+
+private:
+    float m_high;
+    float m_deep;
+    float m_speed;
+    float m_direction;
 };
 
 #endif // AUVDATA_H
